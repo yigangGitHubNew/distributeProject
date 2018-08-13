d8ee6076e18c56beb5b29959f29de123a998c4b0
Administrator <yigang_wu@126.com>
2018-08-13 19:28:11 +0800

    spring cloud config的服务注册到Eureka

--- springCloudConfig/springcloudClient/src/main/java/spring/cloud/config/learning/SpringCloudClientApplication.java
+++ springCloudConfig/springcloudClient/src/main/java/spring/cloud/config/learning/SpringCloudClientApplication.java
@@ -6,11 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableDiscoveryClient
 public class SpringCloudClientApplication {
 
     public static void main(String[] args) {
--- springCloudConfig/springcloudClient/src/main/java/spring/cloud/config/learning/controller/TestController.java
+++ springCloudConfig/springcloudClient/src/main/java/spring/cloud/config/learning/controller/TestController.java
@@ -1,20 +1,18 @@
 package spring.cloud.config.learning.controller;
 
-import groovy.util.logging.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import spring.cloud.config.learning.entity.User;
-import spring.cloud.config.learning.mapper.UserMapper;
 import spring.cloud.config.learning.service.UserService;
 
 import java.util.List;
 
 @RestController
 @RefreshScope
-@Slf4j
 public class TestController {
 
     @Value("${name}")
