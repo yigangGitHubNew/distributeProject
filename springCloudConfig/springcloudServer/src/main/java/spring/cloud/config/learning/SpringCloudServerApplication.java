018f3b4c8e228eb1189c4402b96c6738c2fba04f
Administrator <yigang_wu@126.com>
2018-08-13 19:32:28 +0800

    spring cloud config注册到Eureka中

--- springCloudConfig/springcloudServer/src/main/java/spring/cloud/config/learning/SpringCloudServerApplication.java
+++ springCloudConfig/springcloudServer/src/main/java/spring/cloud/config/learning/SpringCloudServerApplication.java
@@ -1,13 +1,13 @@
 package spring.cloud.config.learning;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class SpringCloudServerApplication {
 
     public static void main(String[] args) {
