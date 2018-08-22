e8c9409042fa05033c4a637c9e5eaf6b7471f386
yigangGitHubNew <yigang_wu@126.com>
2018-08-22 13:36:50 +0800

    断路器的项目源码

---
+++ springCloudConfig/springcloudService-hystrix/src/main/java/spring/cloud/config/learning/config/MyBatisConfig.java
@@ -0,0 +1,64 @@
+package spring.cloud.config.learning.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+@MapperScan(basePackages="spring.cloud.config.learning.mapper")
+public class MyBatisConfig {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String userName;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${mybatis.typeAliasesPackage}")
+    private String typeAliasesPackagel;
+
+    @Value("${mybatis.mapperLocations}")
+    private String mapperLocations;
+
+
+    @Bean
+    public DataSource getDataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        return dataSource;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource ds) throws Exception{
+        SqlSessionFactoryBean sfb = new SqlSessionFactoryBean();
+        sfb.setDataSource(ds);
+        //下边两句仅仅用于*.xml文件，如果整个持久层操作不需要使用到xml文件的话（只用注解就可以搞定），则不加
+        sfb.setTypeAliasesPackage(typeAliasesPackagel);
+        sfb.setMapperLocations(new PathMatchingResourcePatternResolver().getResources(mapperLocations));
+        return sfb.getObject();
+    }
+
+
+
+}
