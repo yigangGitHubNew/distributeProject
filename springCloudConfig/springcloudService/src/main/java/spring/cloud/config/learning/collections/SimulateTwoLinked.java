d8a4fb1f8a1b3d40b0f5592fcbfbe5c902c95b57
yigangGitHubNew <yigang_wu@126.com>
2018-09-26 16:41:03 +0800

    模拟集合类的相关写法

--- springCloudConfig/springcloudService/src/main/java/spring/cloud/config/learning/collections/SimulateTwoLinked.java
+++ springCloudConfig/springcloudService/src/main/java/spring/cloud/config/learning/collections/SimulateTwoLinked.java
@@ -1,4 +1,97 @@
 package spring.cloud.config.learning.collections;
 
 public class SimulateTwoLinked {
+
+    transient int size = 0;
+    Node head;
+
+    public int getSize(){
+        return size;
+    }
+
+    /**
+     * 插入数据
+     * @param object
+     */
+    public void addData(Object object){
+        Node node = new Node(null,object,null);
+        if(size == 0){
+            head = new Node();
+            this.head.setTail(node);
+            node.setPre(head);
+        }else{
+            if(head.getTail() != null){
+                head = head.getTail();
+            }
+            head.setTail(node);
+            node.setPre(head);
+        }
+        size++;
+    }
+
+    /**
+     * 根据索引获取数据
+     * @param index
+     * @return
+     */
+    public Object get(int index){
+        if(index < 0 || index > size-1){
+            System.out.println("双向链表越界");
+            return null;
+        }
+        if(head == null){
+            System.out.println("双向链表还没有数据");
+            return null;
+        }
+        Node temp = head;
+        if(index == 0){
+            return temp.getData();
+        }else{
+            for(int i = 0; i < index-1;i++){
+                temp = temp.getTail();
+            }
+            return temp.getData();
+        }
+    }
+
+
+
+
+    class Node<T>{
+        Node pre;
+        T data;
+        Node tail;
+
+        Node(){}
+
+        Node(Node pre,T data,Node tail){
+            this.pre = pre;
+            this.data = data;
+            this.tail = tail;
+        }
+
+        public Node getPre() {
+            return pre;
+        }
+
+        public void setPre(Node pre) {
+            this.pre = pre;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node getTail() {
+            return tail;
+        }
+
+        public void setTail(Node tail) {
+            this.tail = tail;
+        }
+    }
 }
--- springCloudConfig/springcloudService/src/main/java/spring/cloud/config/learning/collections/TestSimulateTwoLinked.java
+++ springCloudConfig/springcloudService/src/main/java/spring/cloud/config/learning/collections/TestSimulateTwoLinked.java
@@ -1,4 +1,15 @@
 package spring.cloud.config.learning.collections;
 
 public class TestSimulateTwoLinked {
+
+    public static void main(String[] args) {
+        SimulateTwoLinked simulateTwoLinked = new SimulateTwoLinked();
+        simulateTwoLinked.addData("0");
+        simulateTwoLinked.addData("1");
+        simulateTwoLinked.addData("2");
+        simulateTwoLinked.addData("3");
+        simulateTwoLinked.addData("4");
+        System.out.println("双向链表的总数："+simulateTwoLinked.size);
+        System.out.println("获取双向链表的对象值："+simulateTwoLinked.get(3));
+    }
 }
