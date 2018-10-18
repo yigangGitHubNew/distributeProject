95d80f1655ed017342be0f9ca95c168a1550fc92
yigangGitHubNew <yigang_wu@126.com>
2018-10-18 09:35:21 +0800

    模拟集合类的相关写法

---
+++ springCloudConfig/demoboot/src/main/java/com/example/demoboot/DemobootApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobootApplication.class, args);
+	}
+}
