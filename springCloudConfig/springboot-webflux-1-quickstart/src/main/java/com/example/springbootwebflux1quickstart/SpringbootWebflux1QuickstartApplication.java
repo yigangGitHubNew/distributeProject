7609fe3d196036320f861752d41ec3bfb895f4ad
yigangGitHubNew <yigang_wu@126.com>
2018-11-23 15:07:48 +0800

    spring boot webflux项目

---
+++ springCloudConfig/springboot-webflux-1-quickstart/src/main/java/com/example/springbootwebflux1quickstart/SpringbootWebflux1QuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootwebflux1quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebflux1QuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebflux1QuickstartApplication.class, args);
+	}
+}
